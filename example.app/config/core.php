b2a61665da0cb0613d4ce7908fd9bdcc8a6ad3fc
Bence Eros <crystal@cyclonephp.com>
2011-10-05 01:10:54 +0200

    fixing example skeleton app

---
+++ example.app/config/core.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'uri' => array(
+        'base_url' => 'http://local.cyclonephp/'
+    )
+) ;
---
+++ example.app/config/setup.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'default' => array(
+        'readers' => array(new cyclone\config\reader\FileEnv),
+        'writers' => array()
+    )
+);
